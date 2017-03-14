dcc606f76e7ce540579ff8664424f16d8b85a3ff
Irina Shkliarenko <irina.shkliarenko@gmail.com>
2017-03-14 10:14:20 -0700

    testy_v1 Element jest ostatnim elementem w sekwencji

--- src/test/java/edu/iis/mto/bsearch/SearchResultTest.java
+++ src/test/java/edu/iis/mto/bsearch/SearchResultTest.java
@@ -29,8 +29,18 @@ public class SearchResultTest {
     public void FirstElementsInSequence() {
         int key = 2;
         int seq[] = {2, 3, 4, 7};
-        Assert.assertTrue(BinarySearch.search(key, seq).isFound());
-        Assert.assertEquals(1, BinarySearch.search(key, seq).getPosition());
+        int fisrtElement = seq.length - (seq.length-1);
+        
+        Assert.assertEquals(fisrtElement, BinarySearch.search(key, seq).getPosition());
+    }
+    
+    @Test
+    public void LastElementsInSequence() {
+        int key = 7;
+        int seq[] = {2, 3, 4, 7};
+        int lastElement = seq.length;
+        
+        Assert.assertEquals(lastElement, BinarySearch.search(key, seq).getPosition());
     }
 
 }
