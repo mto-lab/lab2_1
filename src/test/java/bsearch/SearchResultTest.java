8bdc26d0b70100de37840ff8b3113a00c6d8780c
oktawianmankowski <198162@edu.p.lodz.pl>
2018-01-28 11:52:07 +0100

    It is the middle element of the sequence

--- src/test/java/bsearch/SearchResultTest.java
+++ src/test/java/bsearch/SearchResultTest.java
@@ -55,4 +55,15 @@ public class SearchResultTest {
         SearchResult searchResult = BinarySearch.search(key, seq);
         assertThat(searchResult.isFound() && searchResult.getPosition() == positionInSequence, is(expectResult));
     }
+
+    @Test
+    public void isInMiddleOfSequence() {
+        final int key = 3;
+        final int[] seq = {1, 2, 3, 4, 5};
+        final int positionInSequence = ((0 + seq.length - 1) / 2) + 1;
+        final boolean expectResult = true;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertThat(searchResult.isFound() && searchResult.getPosition() == positionInSequence, is(expectResult));
+    }
 }
